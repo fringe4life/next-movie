e08eca81e151cac04ba42cabb15d1d922dcabc28
Coinnich <>
2025-02-22 20:34:12 +1300

    fixed moviecard when dark theme is active

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -10,7 +10,8 @@ export default function Loading(){
                         <div className="animate-pulse bg-gray-200 rounded h-6 w-48"/>
                         <div className="animate-pulse bg-gray-200 rounded h-6 w-48"/>
                     </div>
-                </div><div className="flex items-center  space-y-4">
+                </div>
+                <div className="flex items-center  space-y-4">
                     <div className="animate-pulse bg-gray-200 h-37 w-25 rounded-xs"/>
                     <div>
                         <div className="animate-pulse bg-gray-200 rounded h-6 w-48"/>
