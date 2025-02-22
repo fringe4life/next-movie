38e58c69059ca78162f14350fd395c015fe5b2ac
Coinnich <>
2025-02-22 20:41:13 +1300

    removed unneeded interface extension

--- app/components/EmptySection.tsx
+++ app/components/EmptySection.tsx
@@ -1,12 +1,8 @@
 
-interface EmptySectionProps extends React.ComponentPropsWithoutRef<"section"> {
-    
-
-}
 
 
 
-export default function EmptySection({children}: EmptySectionProps){
+export default function EmptySection({children}: React.ComponentPropsWithoutRef<"section">){
     return (
         
             <section className=" max-w-[30ch] flex-1 w-full flex flex-col justify-center justify-items-center items-center gap-2">
