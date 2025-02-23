fc0180c29c32cbdcece174ef9a1bc440f6162219
Coinnich <>
2025-02-23 14:53:39 +1300

    chnaged image layout a little

--- app/components/MovieCard.tsx
+++ app/components/MovieCard.tsx
@@ -29,8 +29,8 @@ export default function MovieCard({toggleMovies, movie:{imdbID, Title, Poster, P
     addToWatchList
 
     return (
-        <section className=" py-6 flex xs:gap-4 gap-2 dark:text-white">
-            <Image className="h-37 w-25 rounded-xs " src={Poster} width={100} height={148} alt={Title} />
+        <section className="py-6 flex xs:gap-4 gap-2 dark:text-white">
+            <Image className="object-cover w-25 rounded-xs" src={Poster} width={100} height={148} alt={Title} />
             <section className="xs:my-[0.625rem]">
                 <header className="flex items-center"> 
                     <h2 className="dark:text-white font-medium whitespace-wrap shrink text-black text-lg  text-ellipses line-clamp-1">{titleToUse}</h2>
@@ -39,7 +39,7 @@ export default function MovieCard({toggleMovies, movie:{imdbID, Title, Poster, P
                 </header>
                 <div className="mt-[0.625rem] mb-[0.5rem] flex gap-1 xs:items-center flex-col xs:flex-row xs:gap-4">
                     <p className="text-xs">{Runtime}</p>
-                    <p className="text-xs xs:max-w-[20ch] xs:text-center">{Genre}</p>
+                    <p className="text-xs xs:max-w-[20ch]">{Genre}</p>
                     <button onClick={() => toggleMovies({imdbID, Title, Poster, Plot, imdbRating, Genre, Runtime})} className="flex items-center gap-1 text-xs hover:cursor-pointer" type="button"><Image className="dark:bg-white fill-white rounded-full" src={iconToUse} alt="add to watchlist icon" width={16} height={16} />Watchlist</button>
                 </div>
                 <p className="text-sm/5  text-gray-500 dark:text-neutral-400  xs:line-clamp-3 text-ellipses">{Plot}</p>
